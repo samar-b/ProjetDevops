34cde28d11b070108078101a757dfbbc603f7d59
samar <samar.belhadj@esprit.tn>
2022-11-01 11:28:42 +0100

    first commit

---
+++ src/main/java/tn/esprit/rh/achat/controllers/StockRestController.java
@@ -0,0 +1,71 @@
+package tn.esprit.rh.achat.controllers;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.mappers.ModelMapper;
+import tn.esprit.rh.achat.dto.DTOStock;
+import tn.esprit.rh.achat.entities.Stock;
+import tn.esprit.rh.achat.services.IStockService;
+
+import java.util.List;
+
+@RestController
+@Api(tags = "Gestion des stocks")
+@RequestMapping("/stock")
+@CrossOrigin("*")
+public class StockRestController {
+
+	@Autowired
+	IStockService stockService;
+
+	// http://localhost:8089/SpringMVC/stock/retrieve-all-stocks
+	@GetMapping("/retrieve-all-stocks")
+	@ResponseBody
+	public List<Stock> getStocks() {
+		return stockService.retrieveAllStocks();
+	}
+
+	// http://localhost:8089/SpringMVC/stock/retrieve-stock/8
+	@GetMapping("/retrieve-stock/{stock-id}")
+	@ResponseBody
+	public Stock retrieveStock(@PathVariable("stock-id") Long stockId) {
+		return stockService.retrieveStock(stockId);
+	}
+
+	// http://localhost:8089/SpringMVC/stock/add-stock
+	@PostMapping("/add-stock")
+	@ResponseBody
+	public Stock addStock(@RequestBody DTOStock s) {
+		Stock stock = new Stock(s.getLibelleStock(),s.getQte(),s.getQteMin());
+		return stockService.addStock(stock);
+	}
+
+	@DeleteMapping("/remove-stock/{stock-id}")
+	@ResponseBody
+	public void removeStock(@PathVariable("stock-id") Long stockId) {
+		stockService.deleteStock(stockId);
+	}
+
+	// http://localhost:8089/SpringMVC/stock/modify-stock
+	@PutMapping("/modify-stock")
+	@ResponseBody
+	public Stock modifyStock(@RequestBody DTOStock s) {
+		Stock stock = new Stock(s.getIdStock(),s.getLibelleStock(),s.getQte(),s.getQteMin(),s.getProduits());
+		return stockService.updateStock(stock);
+	}
+
+	/*
+	 * Spring Scheduler : Comparer QteMin tolérée (à ne pa dépasser) avec
+	 * Quantité du stock et afficher sur console la liste des produits inférieur
+	 * au stock La fct schédulé doit obligatoirement etre sans paramètres et
+	 * sans retour (void)
+	 */
+	// http://localhost:8089/SpringMVC/stock/retrieveStatusStock
+	// @Scheduled(fixedRate = 60000)
+	// @Scheduled(fixedDelay = 60000)
+	//@Scheduled(cron = "*/60 * * * * *")
+	//@GetMapping("/retrieveStatusStock")
+
+}
